2c37cb21b5785aee0494a792b6703b1cdfa19c80
Jim Balhoff <jim@balhoff.org>
2024-08-27 15:25:21 -0400

    Clean up some lint issues.

--- packages/actor-query-operation-paths/lib/ActorQueryOperationPaths.ts
+++ packages/actor-query-operation-paths/lib/ActorQueryOperationPaths.ts
@@ -1,14 +1,15 @@
-import { ActorQueryOperationTypedMediated, IActorQueryOperationTypedMediatedArgs } from '@comunica/bus-query-operation';
-import { IActorTest } from '@comunica/core';
-import type { IActionContext, IQueryOperationResultPaths } from '@comunica/types';
-import { ArrayIterator, AsyncIterator } from 'asynciterator';
-import { Path } from '@comunica/path-factory';
-import { Algebra } from 'sparqlalgebrajs';
-import {Utils} from "./Utils"
-import * as RDF from '@rdfjs/types';
-import { DataFactory } from 'rdf-data-factory';
 import { BindingsFactory } from '@comunica/bindings-factory';
-// import { Parser as SparqlParser, Generator as SparqlGenerator } from 'sparqljs'
+import type { IActorQueryOperationTypedMediatedArgs } from '@comunica/bus-query-operation';
+import { ActorQueryOperationTypedMediated } from '@comunica/bus-query-operation';
+import type { IActorTest } from '@comunica/core';
+import { Path } from '@comunica/path-factory';
+import type { IActionContext, IQueryOperationResultPaths } from '@comunica/types';
+import type * as RDF from '@rdfjs/types';
+import type { AsyncIterator } from 'asynciterator';
+import { ArrayIterator } from 'asynciterator';
+import { DataFactory } from 'rdf-data-factory';
+import type { Algebra } from 'sparqlalgebrajs';
+import { Utils } from './Utils';
 
 /**
  * A [Query Operation](https://github.com/comunica/comunica/tree/master/packages/bus-query-operation) actor that handles SPARQL paths operations.
@@ -21,26 +22,28 @@ export class ActorQueryOperationPaths extends ActorQueryOperationTypedMediated<A
   }
 
   public async testOperation(_operation: Algebra.Paths, _context: IActionContext): Promise<IActorTest> {
-    // var start = _operation.start.value;
-    // var end = _operation.end.value;
-
     // No paths between two of the same nodes.
-    
     return _operation.type === 'paths';
-
   }
 
   public async runOperation(operation: Algebra.Paths, context: IActionContext):
   Promise<IQueryOperationResultPaths> {
     const DF = new DataFactory();
-    let start: RDF.Term = operation.start.value ? this.pathValue(operation.start) : DF.variable(operation.start.var.value);
-    let via: RDF.Term =  ("value" in operation.via) ? this.pathValue(operation.via) : DF.variable(operation.via.var.value);
-    let end: RDF.Term = operation.end.value ? this.pathValue(operation.end) : DF.variable(operation.end.var.value);
+    const start: RDF.Term = operation.start.value ?
+      this.pathValue(operation.start) :
+      DF.variable(operation.start.var.value);
+    const via: RDF.Term = ('value' in operation.via) ?
+      this.pathValue(operation.via) :
+      DF.variable(operation.via.var.value);
+    const end: RDF.Term = operation.end.value ?
+      this.pathValue(operation.end) :
+      DF.variable(operation.end.var.value);
     // Run paths algorithm depending on type of query (cyclic or shortest/all paths?).
-    var utils = new Utils(this.mediatorQueryOperation, context); 
-    let stream = operation.cyclic === true ?
-      (await this.cyclicPaths(start, via, end, utils)) :
-      (await this.paths(start, via, end, operation, utils));
+    const utils = new Utils(this.mediatorQueryOperation, context);
+    // FIXME if this comparison is simplified, doesn't work correctly; need to find out why
+    const stream = operation.cyclic === true ?
+        (await this.cyclicPaths(start, via, end, utils)) :
+        (await this.paths(start, via, end, operation, utils));
 
     const output: AsyncIterator<RDF.Path> = new ArrayIterator<RDF.Path>(stream, {autoStart: false});
     return {
@@ -50,15 +53,14 @@ export class ActorQueryOperationPaths extends ActorQueryOperationTypedMediated<A
   }
 
   private pathValue(pathValue: any): RDF.Term {
-
     if (pathValue.value && !Array.isArray(pathValue.value)) {
       return pathValue.value.value.value;
     }
-    throw new Error("Pathfinder cannot process graph patterns yet.")
+    throw new Error('Pathfinder cannot process graph patterns yet.');
   }
 
-  private async paths(start: RDF.Term, via: RDF.Term, end: RDF.Term, operation: Algebra.Operation, utils: Utils)
-  : Promise<RDF.Path[]> {
+  private async paths(start: RDF.Term, via: RDF.Term, end: RDF.Term, operation: Algebra.Operation, utils: Utils):
+  Promise<RDF.Path[]> {
     const DF = new DataFactory();
     const BF = new BindingsFactory();
 
@@ -66,82 +68,74 @@ export class ActorQueryOperationPaths extends ActorQueryOperationTypedMediated<A
     const traversed = new Set<RDF.Term>();
     const queue: [RDF.Term, RDF.Bindings[]][] = [[start, []]];
     const pathsArr: RDF.Path[] = [];
-    
+
     // Loop while there are nodes to process in the queue.
     while (queue.length > 0) {
-      const [currentNode, path] = queue.shift()!;
-  
+      const [ currentNode, path ] = queue.shift()!;
       // Query for all neighboring nodes of the current node.
       const outgoingEdges = (await utils.query(currentNode, via)) || [];
-  
       for (const neighborBinding of outgoingEdges) {
         // Get the neighbor node from the binding and skip if undefined.
         const neighbor = neighborBinding.get('o');
-        if (!neighbor) continue;
-    
-        var order = BF.bindings([
-          [DF.variable('s'), neighborBinding.get('s') ?? DF.namedNode('s')],
-          [DF.variable('p'), neighborBinding.get('p') ?? DF.namedNode('p')],
-          [DF.variable('o'), neighborBinding.get('o') ?? DF.namedNode('o')]])
-        const newPath = [...path, order];
+        if (!neighbor) {
+          continue;
+        }
+        const order = BF.bindings([
+          [ DF.variable('s'), neighborBinding.get('s') ?? DF.namedNode('s') ],
+          [ DF.variable('p'), neighborBinding.get('p') ?? DF.namedNode('p') ],
+          [ DF.variable('o'), neighborBinding.get('o') ?? DF.namedNode('o') ],
+        ]);
+        const newPath = [ ...path, order ];
         // Create a new path including the current neighborBinding.
         // const newPath = [...path, neighborBinding];
-  
         // Check if the neighbor node is the target end node.
-        if (end.termType === "NamedNode" && neighbor.value === end.value) {
+        if (end.termType === 'NamedNode' && neighbor.value === end.value) {
           pathsArr.push(new Path(newPath));
           // If we only want the shortest path, return immediately.
-          if (operation.all !== true) return pathsArr;
-  
-        } else if (neighbor.termType !== "Literal" && (!traversed.has(neighbor) || operation.all === true)) {
+          if (operation.all !== true) {
+            return pathsArr;
+          }
+        } else if (neighbor.termType !== 'Literal' && (!traversed.has(neighbor) || operation.all === true)) {
           // If the neighbor is not a literal and has not been traversed,
           // or if we want all paths, add the neighbor to the queue for further processing.
-          queue.push([neighbor, newPath]);
+          queue.push([ neighbor, newPath ]);
         }
       }
-  
       // Mark the current node as traversed.
       traversed.add(currentNode);
     }
-    
     // Return all found paths.
     return pathsArr;
   }
 
-  private async cyclicPaths(startNode: RDF.Term, via: RDF.Term, endNode: RDF.Term, utils: Utils)
-  : Promise<RDF.Path[]> {
+  private async cyclicPaths(startNode: RDF.Term, via: RDF.Term, endNode: RDF.Term, utils: Utils): Promise<RDF.Path[]> {
     // Initialize data structures
-    const traversed = new Set<String>();
+    const traversed = new Set<string>();
     const pathArr: RDF.Bindings[] = [];
-    var pathsArr: RDF.Path[] = [];
-  
+    const pathsArr: RDF.Path[] = [];
+
     async function dfs(node: RDF.Term, end?: RDF.Term, neighborBinding?: RDF.Bindings): Promise<void> {
       // If a neighborBinding is provided, push it to the path array.
       neighborBinding && pathArr.push(neighborBinding);
-    
       // If the current node has already been traversed.
       if (traversed.has(node.value)) {
         // If there is an end node specified and the current node is the end node OR if end node doesnt matter,
         // push a copy of pathArr to the pathsArr.
-        if ((end && node.equals(end)) || !end) {
-          pathsArr.push(new Path(pathArr.slice()));
+        if ((end && node.equals(end)) ?? !end) {
+          pathsArr.push(new Path([ ...pathArr ]));
         }
         // Remove the last element from pathArr since we are backtracking.
         pathArr.pop();
         return;
       }
-    
       // Mark the current node as traversed.
-      traversed.add(node.value);  
-    
+      traversed.add(node.value);
       try {
         // Query for all outgoing edges from the current node.
         const outgoingEdges = await utils.query(node, via) || [];
-    
         // Process each outgoing edge (neighbor).
         for (const neighborBinding of outgoingEdges) {
           const neighborTerm = neighborBinding.get('o');
-    
           // If the neighboring node exists, recursively call dfs on it.
           if (neighborTerm) {
             await dfs(neighborTerm, end, neighborBinding);
@@ -149,7 +143,6 @@ export class ActorQueryOperationPaths extends ActorQueryOperationTypedMediated<A
         }
       } catch (error) {
         console.error("Error in DFS:", error);
-    
       } finally {
         // Remove the last element from pathArr since we are backtracking.
         pathArr.pop();
@@ -157,22 +150,17 @@ export class ActorQueryOperationPaths extends ActorQueryOperationTypedMediated<A
         traversed.delete(node.value);
       }
     }
-    
-  
-    if ( startNode.termType == 'NamedNode' && endNode.termType == 'NamedNode' ) {
+    if (startNode.termType === 'NamedNode' && endNode.termType === 'NamedNode') {
       // If start node is specified, only search from that node
       await dfs(startNode);
       return pathsArr;
-    } else {
-      // // Otherwise, search from all nodes
-      // for (const node in graph) {
-      //   if (!traversed.has(node)) {
-      //     dfs(node, []);
-      //   }
-      // }
     }
+    // // Otherwise, search from all nodes
+    // for (const node in graph) {
+    //   if (!traversed.has(node)) {
+    //     dfs(node, []);
+    //   }
+    // }
     return pathsArr;
-  
   }
-
 }
--- packages/actor-query-operation-paths/lib/Utils.ts
+++ packages/actor-query-operation-paths/lib/Utils.ts
@@ -1,56 +1,50 @@
-import * as RDF from '@rdfjs/types';
+import type { QuerySourceSkolemized } from '@comunica/actor-context-preprocess-query-source-skolemize';
 import { ActorQueryOperation } from '@comunica/bus-query-operation';
-import { Algebra, Factory, translate } from 'sparqlalgebrajs';
-import { QuerySourceSkolemized } from '@comunica/actor-context-preprocess-query-source-skolemize';
-import { IActionContext, IActionContextKey } from '@comunica/types'
-
+import type { IActionContext, IActionContextKey } from '@comunica/types';
+import type * as RDF from '@rdfjs/types';
+import type { Algebra } from 'sparqlalgebrajs';
+import { Factory, translate } from 'sparqlalgebrajs';
 
 export class Utils {
+  private readonly Factory: Factory;
+  private readonly mediatorQueryOperation: any;
+  private readonly context: IActionContext;
+  private readonly sources: { context: unknown; source: QuerySourceSkolemized }[];
 
-  private Factory: Factory;
-  private mediatorQueryOperation: any;
-  private context: IActionContext;
-  private sources: Array<{ context: unknown; source: QuerySourceSkolemized }>;
-
-  public constructor( mediatorQueryOperation: any, context: IActionContext, F?: Factory) {
-    this.Factory = F || new Factory();
+  public constructor(mediatorQueryOperation: any, context: IActionContext, F?: Factory) {
+    this.Factory = F ?? new Factory();
     this.mediatorQueryOperation = mediatorQueryOperation;
     this.context = context;
 
     // Parse all sources:
-    let key: IActionContextKey<unknown> = {
-      name: '@comunica/bus-query-operation:querySources'
-    }
+    const key: IActionContextKey<unknown> = {
+      name: '@comunica/bus-query-operation:querySources',
+    };
     try {
-      this.sources = context.get(key) as Array<{ context: unknown; source: QuerySourceSkolemized }>;
+      this.sources = context.get(key) as { context: unknown; source: QuerySourceSkolemized }[];
     } catch {
       // Cannot run queries without sources.
-      throw new Error("Error parsing sources in Query Operation Paths Actor.");
+      throw new Error('Error parsing sources in Query Operation Paths Actor.');
     }
   }
 
   public async query(sub: RDF.Term, pred: RDF.Term): Promise<RDF.Bindings[]> {
-    // console.log('sub sexy', sub)
-    // console.log('pred sexy', pred);
-    if (pred.termType == 'Variable' || pred.value == '?p') {
-      var q = `SELECT ?s ?p ?o WHERE {VALUES ?s { <${sub.value}> } ?s ?p ?o .}`;
+    let q: string;
+    if (pred.termType === 'Variable' || pred.value === '?p') {
+      q = `SELECT ?s ?p ?o WHERE {VALUES ?s { <${sub.value}> } ?s ?p ?o .}`;
     } else {
       q = `SELECT ?s ?p ?o WHERE {VALUES ?s { <${sub.value}> } VALUES ?p { <${pred.value}> } ?s ?p ?o .}`;
     }
-    
-    var unions: Algebra.Operation[] = [];
+    const unions: Algebra.Operation[] = [];
     for (const source of this.sources) {
       const project = translate(q);
-      project.input.input.at(-1).patterns[0].metadata = {
-        scopedSource: source
-      };
+      project.input.input.at(-1).patterns[0].metadata = { scopedSource: source };
       unions.push(project);
     }
     const query = this.Factory.createUnion(unions);
-    
-    var outgoingEdges = ActorQueryOperation.getSafeBindings(
-      await this.mediatorQueryOperation.mediate({ operation: query, context: this.context })) || [];
-  
+    const outgoingEdges = ActorQueryOperation.getSafeBindings(
+      await this.mediatorQueryOperation.mediate({ operation: query, context: this.context }),
+    ) || [];
     return outgoingEdges.bindingsStream.toArray();
   }
 }
